14ee1458408929993e9a2448e800a9f7b17382c6
Aliaksei <busleika.aliaksei@student.ehu.lt>
2023-12-03 23:20:58 +0100

    2nd commit

--- test/TestRunner.java
+++ test/TestRunner.java
@@ -1,5 +1,6 @@
+import org.junit.Assert;
 import org.junit.Test;
-
+import java.io.FileNotFoundException;
 import java.util.Enumeration;
 import java.util.Locale;
 import java.util.MissingResourceException;
@@ -12,8 +13,8 @@ public class TestRunner {
     private static class Result {
         private int errorLines;
         private double sum;
-        private /*static final*/ String RESULT = "sum = ";
-        private /*static final*/ String ERROR = "error-lines = ";
+        private static final String RESULT = "sum = ";
+        private static final String ERROR = "error-lines = ";
 
         public Result(){
             this.errorLines = 0;
@@ -43,31 +44,66 @@ public class TestRunner {
         final String KEY_REG_EXP = "index(.*)";
         final String NUM_REG_EXP = "[1-9]\\d*";
 
+        int tailIndex = 1;
+        int errors = 0;
+        String value = "value";
+        Result result = new Result();
 
         while(keys.hasMoreElements()){
             String key = keys.nextElement();
-
             Pattern pat = Pattern.compile(KEY_REG_EXP);
             Matcher mat = pat.matcher(key);
 
-            int tailIndex = 1;
 
             if(mat.matches()){
 
-                //group???
                 String iString = mat.group(tailIndex);
-                String jStringgit ;
+                String jString = rb.getString(key).trim();
+
+                Matcher iMatcher = pat.matcher(iString);
+                Pattern pattern = Pattern.compile(NUM_REG_EXP);
+                Matcher jMatcher = pattern.matcher(jString);
+
+                if(iMatcher.matches() && jMatcher.matches()){
+                    String valueIJ = value + iString + jString;
+
+                    try {
+                        System.out.println(valueIJ);
+
+                    }catch (IndexOutOfBoundsException | MissingResourceException e){
+                        errors++;
+                    }
+                }else {
+                    errors++;
+                }
             }
-
         }
+        return result;
+    }
+    private double DELTA = 0.000001;
 
 
+    @Test
+    public void testMainScenario1() throws FileNotFoundException {
+        Result result1 = getResult("/home/ave/university-2/practical7-v2/src/in1");
+        Assert.assertEquals(3, result1.getErrorLines());
+        Assert.assertEquals(8.24, result1.getSum(), DELTA);
+    }
+
+    @Test
+    public void testMainScenario2() throws FileNotFoundException {
+        Result result1 = getResult("/home/ave/university-2/practical7-v2/src/in2");
+        Assert.assertEquals(9, result1.getErrorLines());
+        Assert.assertEquals(30.242, result1.getSum(), DELTA);
+    }
+
+    @Test
+    public void testMainScenario3() throws FileNotFoundException {
+        Result result1 = getResult("/home/ave/university-2/practical7-v2/src/in3");
+        Assert.assertEquals(0, result1.getErrorLines());
+        Assert.assertEquals(1.9, result1.getSum(), DELTA);
     }
 
 
 
-
-
-
-    @Test
 }
